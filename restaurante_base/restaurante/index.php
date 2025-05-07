02068de49b1306297cf56b667fc2445177588805
andresidaz <afroa@uniboyaca.edu.co>
2025-05-06 21:31:53 -0500

    actualizacion de menu principal

--- restaurante_base/restaurante/index.php
+++ restaurante_base/restaurante/index.php
@@ -2,48 +2,56 @@
 <html lang="es">
 <head>
     <meta charset="UTF-8">
-    <title>Menú Principal</title>
+    <title>Menú con Submenú</title>
     <link rel="stylesheet" href="css/index.css">
 </head>
 <body>
 
-    <h1>🍽️ Menú Principal del Restaurante</h1>
-    
-    <!-- Menú de Navegación -->
-    <div class="navbar">
-        <ul>
-            <li><a href="#">Gestión de Platos</a>
-                <ul>
+    <div class="header">
+        <h1>🍽️ Menú Principal del Restaurante</h1>
+    </div>
+
+    <nav class="navbar">
+        <ul class="menu">
+            <li class="menu-item">
+                <a href="#">Gestión de Platos</a>
+                <ul class="submenu">
                     <li><a href="router.php?controller=plato&action=listar">Ver Platos</a></li>
                     <li><a href="router.php?controller=plato&action=crear">Registrar Nuevo Plato</a></li>
                 </ul>
             </li>
-            <li><a href="#">Gestión de Categorías</a>
-                <ul>
+            <li class="menu-item">
+                <a href="#">Gestión de Categorías</a>
+                <ul class="submenu">
                     <li><a href="router.php?controller=categoria&action=listar">Ver Categorías</a></li>
                     <li><a href="router.php?controller=categoria&action=crear">Registrar Nueva Categoría</a></li>
                 </ul>
             </li>
-            <li><a href="#">Gestión de Mesas</a>
-                <ul>
+            <li class="menu-item">
+                <a href="#">Gestión de Mesas</a>
+                <ul class="submenu">
                     <li><a href="router.php?controller=mesa&action=listar">Ver Mesas</a></li>
                     <li><a href="router.php?controller=mesa&action=crear">Registrar Nueva Mesa</a></li>
                 </ul>
             </li>
-            <li><a href="#">Órdenes</a>
-                <ul>
+            <li class="menu-item">
+                <a href="#">Órdenes</a>
+                <ul class="submenu">
                     <li><a href="router.php?controller=orden&action=listar">Ver Órdenes</a></li>
                     <li><a href="router.php?controller=orden&action=crear">Registrar Nueva Orden</a></li>
                 </ul>
             </li>
-            <li><a href="#">Reportes</a>
-                <ul>
+            <li class="menu-item">
+                <a href="#">Reportes</a>
+                <ul class="submenu">
                     <li><a href="router.php?controller=reporte&action=general">Órdenes No Anuladas</a></li>
                     <li><a href="router.php?controller=reporte&action=anuladas">Órdenes Anuladas</a></li>
                 </ul>
             </li>
         </ul>
-    </div>
+    </nav>
+
+    <script src="js/index.js"></script>
 
 </body>
 </html>
